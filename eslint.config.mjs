8b1144710216d9ad44b23f0d9455d9458d53d9f5
alishersheraliyev <81514415+alishersheraliyev@users.noreply.github.com>
2025-01-06 21:21:12 +0500

    Setup config files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,14 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "standart",
+    "plugin:tailwindcss/recommended",
+    "prettier"
+  ),
+  ...compat.plugins("import"),
 ];
 
 export default eslintConfig;
