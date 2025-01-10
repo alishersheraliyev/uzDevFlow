d2ee5b958caa5abe2bc26ef038f6bd778da7e737
alishersheraliyev <81514415+alishersheraliyev@users.noreply.github.com>
2025-01-10 22:00:22 +0500

    addm question cards

--- lib/validations.ts
+++ lib/validations.ts
@@ -5,9 +5,10 @@ export const SignInSchema = z.object({
     .string()
     .min(1, { message: "Email is required" })
     .email({ message: "Please provide a valid email address." }),
+
   password: z
     .string()
-    .min(6, { message: "Password must be at least 6 characters long." })
+    .min(6, { message: "Password must be at least 6 characters long. " })
     .max(100, { message: "Password cannot exceed 100 characters." }),
 });
 
@@ -47,4 +48,22 @@ export const SignUpSchema = z.object({
     .regex(/[^a-zA-Z0-9]/, {
       message: "Password must contain at least one special character.",
     }),
+});
+
+export const AskQuestionSchema = z.object({
+  title: z
+    .string()
+    .min(5, { message: "Title is required." })
+    .max(100, { message: "Title cannot exceed 100 characters." }),
+
+  content: z.string().min(1, { message: "Body is required." }),
+  tags: z
+    .array(
+      z
+        .string()
+        .min(1, { message: "Tag is required." })
+        .max(30, { message: "Tag cannot exceed 30 characters." })
+    )
+    .min(1, { message: "At least one tag is required." })
+    .max(3, { message: "Cannot add more than 3 tags." }),
 });
